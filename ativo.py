4153dcac1a3d221956f582f0bf00fbdd2f2419b1
Jorge Oliveira <jorgeoliv32@gmail.com>
2022-04-17 11:15:14 -0300

    input message updated

--- ativo.py
+++ ativo.py
@@ -6,7 +6,7 @@ PORT = 3000 # porta do passivo
 s = socket(AF_INET, SOCK_STREAM) #inicializa o socket
 s.connect((HOST,PORT))  # contecta o ativo com o passivo
 
-msg = input("Digite sua mensagem: ") # usuário digita a mensagem
+msg = input("Digite sua mensagem ou escreva '!' para encerrar o programa: ") # usuário digita a mensagem
 while msg != '!':
     s.send(str.encode(msg)) # envia a mensagem para o passivo
     data = s.recv(1024) # recebe a resposta
